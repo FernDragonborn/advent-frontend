cdb9ae11853306d07da300b1997403b3d649875b
Bohdan Khomych <bod.khomych@gmail.com>
2024-11-29 13:59:37 +0200

    update phone input

--- src/app/components/formControls/PhoneInputField.jsx
+++ src/app/components/formControls/PhoneInputField.jsx
@@ -3,9 +3,13 @@
 import MaskedInput from 'react-text-mask';
 import clsx from 'clsx';
 
-import styles from '@/styles/components/formControls/PhoneInputField.module.scss';
 import { SmartPhoneSvg } from '@/assets/images/svgs';
-import { PHONE_INPUT_MASK } from '@/constants';
+import {
+  FIXED_PHONE_MASK_CHARACTER_NUMBER,
+  PHONE_INPUT_MASK,
+} from '@/constants';
+import { formatPhone } from '@/utils';
+import styles from '@/styles/components/formControls/PhoneInputField.module.scss';
 
 const PhoneInputField = ({
   label,
@@ -17,6 +21,8 @@ const PhoneInputField = ({
   ref,
   ...props
 }) => {
+  const inputValue = String(props?.value);
+
   return (
     <div
       className={clsx(
@@ -29,18 +35,25 @@ const PhoneInputField = ({
         {label || 'Номер'}
       </label>
 
-      <MaskedInput
-        {...props}
-        className={styles.input}
-        aria-invalid={error ? 'true' : 'false'}
-        id={name}
-        name={name}
-        placeholder={placeholder || '+38 (099) 999 9999'}
-        type="tel"
-        mask={PHONE_INPUT_MASK}
-        guide={true}
-        placeholderChar="-"
-      />
+      <div className={styles.inputWrapper}>
+        <span className={styles.countryCode}>+38 (0</span>
+        <MaskedInput
+          {...props}
+          onChange={({ currentTarget }) => {
+            props?.onChange?.(formatPhone(currentTarget.value));
+          }}
+          value={inputValue.slice(FIXED_PHONE_MASK_CHARACTER_NUMBER)}
+          className={styles.input}
+          aria-invalid={error ? 'true' : 'false'}
+          id={name}
+          name={name}
+          placeholder={placeholder || '99) 999 9999'}
+          type="tel"
+          mask={PHONE_INPUT_MASK}
+          guide={true}
+          placeholderChar="-"
+        />
+      </div>
 
       {error?.message && (
         <p className={styles.error} role="alert">
