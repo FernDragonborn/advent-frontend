8bfeb6b3dc20da35dadf33f9ca11fdd201d7932d
Bohdan Khomych <bod.khomych@gmail.com>
2024-11-30 22:23:46 +0200

    add meta pixel

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -7,6 +7,7 @@ export { default as Socials } from './common/Socials';
 export { default as CalendarCountdown } from './common/CalendarCountdown';
 export { default as Loader } from './common/Loader';
 export { default as Recaptcha } from './common/Recaptcha';
+export { default as FacebookPixel } from './common/FacebookPixel';
 
 export { default as Header } from './layout/Header';
 export { default as Footer } from './layout/Footer';
